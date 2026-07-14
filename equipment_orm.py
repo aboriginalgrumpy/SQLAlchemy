f0b294e25ea64707896ab0bca24922e2d3c46ebf
aboriginalgrumpy <aaboriginalofficial@gmail.com>
2026-07-14 09:52:10 +0800

    Add Github Actions workflow testing

--- equipment_orm.py
+++ equipment_orm.py
@@ -17,11 +17,16 @@ load_dotenv()
 # class Base(DeclarativeBase):  /this is the 2.0 sqlalchemy orm
 #     pass  /both have the same function
 
-db_user = os.getenv("DB_USER")
+db_user = os.getenv("DB_USER") 
 db_pass = os.getenv("DB_PASS")
 db_host = os.getenv("DB_HOST")
 db_port = os.getenv("DB_PORT")
 db_name = os.getenv("DB_NAME")
+
+# if os.getenv("DB_USER"):
+#     print( os.getenv("DB_USER"))
+
+# if 
 
 DATABASE_URL = f"postgresql+psycopg2://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}"
 
